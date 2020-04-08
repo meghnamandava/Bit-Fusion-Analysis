7dd2e024f452501cb8c263958f2d46afe8d08334
meghna <meghnamandava@berkeley.edu>
2020-04-07 17:50:50 -0700

    got hls reports

--- hls/bitfusion.c
+++ hls/bitfusion.c
@@ -1,61 +1,69 @@
 #include "ap_cint.h"
 
-void bitfusion(uint4 in, uint4 weight, uint1 s_in, uint1 s_weight, uint3 in_width, uint3 weight_width, uint8 psum) {
+void bitfusion(uint4 in, uint4 weight, uint1 s_in, uint1 s_weight, uint3 in_width, uint3 weight_width, uint8 *psum) {
+#pragma HLS INTERFACE ap_vld port=psum
+#pragma HLS INTERFACE ap_vld port=weight_width
+#pragma HLS INTERFACE ap_vld port=in_width
+#pragma HLS INTERFACE ap_vld port=s_weight
+#pragma HLS INTERFACE ap_vld port=s_in
+#pragma HLS INTERFACE ap_vld port=weight
+#pragma HLS INTERFACE ap_vld port=in
     if (s_in == 1 && s_weight == 1) {
         if (in_width == 1 && weight_width == 4) {
-            psum = (uint8) ((int2) in * (int4) weight);
+            *psum = (uint8) ((int2) in * (int4) weight);
         } else if (in_width == 2 && weight_width == 4) {
-            psum = (uint8) ((int2) in * (int4) weight);
+            *psum = (uint8) ((int2) in * (int4) weight);
         } else if (in_width == 4 && weight_width == 1) {
-            psum = (uint8) ((int4) in * (int2) weight);
+            *psum = (uint8) ((int4) in * (int2) weight);
         } else if (in_width == 4 && weight_width == 2) {
-            psum = (uint8) ((int4) in * (int2) weight);
+            *psum = (uint8) ((int4) in * (int2) weight);
         } else if (in_width == 4 && weight_width == 4) {
-            psum = (uint8) ((int4) in * (int4) weight);
+            *psum = (uint8) ((int4) in * (int4) weight);
         } else {
-            psum = (uint8) ((int2) in * (int2) weight);
+            *psum = (uint8) ((int2) in * (int2) weight);
         }
     } else if (s_in == 0 && s_weight == 1) {
         if (in_width == 1 && weight_width == 4) {
-            psum = (uint8) ((uint2) in * (int4) weight);
+            *psum = (uint8) ((uint2) in * (int4) weight);
         } else if (in_width == 2 && weight_width == 4) {
-            psum = (uint8) ((uint2) in * (int4) weight);
+            *psum = (uint8) ((uint2) in * (int4) weight);
         } else if (in_width == 4 && weight_width == 1) {
-            psum = (uint8) ((uint4) in * (int2) weight);
+            *psum = (uint8) ((uint4) in * (int2) weight);
         } else if (in_width == 4 && weight_width == 2) {
-            psum = (uint8) ((uint4) in * (int2) weight);
+            *psum = (uint8) ((uint4) in * (int2) weight);
         } else if (in_width == 4 && weight_width == 4) {
-            psum = (uint8) ((uint4) in * (int4) weight);
+            *psum = (uint8) ((uint4) in * (int4) weight);
         } else {
-            psum = (uint8) ((uint2) in * (int2) weight);
+            *psum = (uint8) ((uint2) in * (int2) weight);
         }   
     } else if (s_in == 0 && s_weight == 0) { 
         if (in_width == 1 && weight_width == 4) {
-            psum = (uint8) ((uint2) in * (uint4) weight);
+            *psum = (uint8) ((uint2) in * (uint4) weight);
         } else if (in_width == 2 && weight_width == 4) {
-            psum = (uint8) ((uint2) in * (uint4) weight);
+            *psum = (uint8) ((uint2) in * (uint4) weight);
         } else if (in_width == 4 && weight_width == 1) {
-            psum = (uint8) ((uint4) in * (uint2) weight);
+            *psum = (uint8) ((uint4) in * (uint2) weight);
         } else if (in_width == 4 && weight_width == 2) {
-            psum = (uint8) ((uint4) in * (uint2) weight);
+            *psum = (uint8) ((uint4) in * (uint2) weight);
         } else if (in_width == 4 && weight_width == 4) {
-            psum = (uint8) ((uint4) in * (uint4) weight);
+            *psum = (uint8) ((uint4) in * (uint4) weight);
         } else {
-            psum = (uint8) ((uint2) in * (uint2) weight);
+            *psum = (uint8) ((uint2) in * (uint2) weight);
         } 
     } else if (s_in == 1 && s_weight == 0) {
         if (in_width == 1 && weight_width == 4) {
-            psum = (uint8) ((int2) in * (uint4) weight);
+            *psum = (uint8) ((int2) in * (uint4) weight);
         } else if (in_width == 2 && weight_width == 4) {
-            psum = (uint8) ((int2) in * (uint4) weight);
+            *psum = (uint8) ((int2) in * (uint4) weight);
         } else if (in_width == 4 && weight_width == 1) {
-            psum = (uint8) ((int4) in * (uint2) weight);
+            *psum = (uint8) ((int4) in * (uint2) weight);
         } else if (in_width == 4 && weight_width == 2) {
-            psum = (uint8) ((int4) in * (uint2) weight);
+            *psum = (uint8) ((int4) in * (uint2) weight);
         } else if (in_width == 4 && weight_width == 4) {
-            psum = (uint8) ((int4) in * (uint4) weight);
+            *psum = (uint8) ((int4) in * (uint4) weight);
         } else {
-            psum = (uint8) ((int2) in * (uint2) weight);
+            *psum = (uint8) ((int2) in * (uint2) weight);
         }
     }
+    printf("i: %d, w: %d, psum: %d\n", in, weight, *psum);
 } 
--- hls/bitfusion_tb.c
+++ hls/bitfusion_tb.c
@@ -3,20 +3,25 @@
 #include <stdio.h>
 #include <assert.h>
 
-void bitfusion(uint4 in, uint4 weight, uint1 s_in, uint1 s_weight, uint3 in_width, uint3 weight_width, uint8 psum);
+void bitfusion(uint4 in, uint4 weight, uint1 s_in, uint1 s_weight, uint3 in_width, uint3 weight_width, uint8 *psum);
 
 int main() {
-    uint4 i;
-    uint4 j;
-    uint3 in_width = 1;
-    uint3 weight_width = 1;
-    uint1 s_weight = 0;
-    uint1 s_in = 0;
+    static uint4 i = 0;
+    static uint4 j = 0;
+    static int4 k = 0;
+    static int4 l = 0;
+    static uint3 in_width = 1;
+    static uint3 weight_width = 1;
+    static uint1 s_weight = 0;
+    static uint1 s_in = 0;
     uint8 psum;
     for (i = 0; i <= 1; i++) {
         for (j = 0; j <= 1; j++) {
-            bitfusion(i, j, s_in, s_weight, in_width, weight_width, psum);
-            if (psum != i*j) return 1;                
+            bitfusion(i, j, s_in, s_weight, in_width, weight_width, &psum);
+            if (psum != i*j) {
+                printf("i: %d, j: %d, psum: %d\n", i, j, psum);
+            	return 1;
+            }
         }
     }
 
@@ -26,31 +31,46 @@ int main() {
     s_in = 0;
     for (i = 0; i <= 3; i++) {
         for (j = 0; j <= 3; j++) {
-            bitfusion(i, j, s_in, s_weight, in_width, weight_width, psum);
+            bitfusion(i, j, s_in, s_weight, in_width, weight_width, &psum);
             if (psum != i*j) return 1;                
         }
     }
 
-    in_width = 4;
+    /*in_width = 4;
     weight_width = 4;
     s_weight = 0;
     s_in = 0;
-    for (i = 0; i <= 15; i++) {
-        for (j = 0; j <= 15; j++) {
+    for (i = 13; i <= 15; i++) {
+        for (j = 13; j <= 14; j++) {
             bitfusion(i, j, s_in, s_weight, in_width, weight_width, psum);
-            if (psum != i*j) return 1;                
+            if (psum != i*j) {
+            	//printf("fail\n");
+            	return 1;
+            } else {
+            	//printf("psum: %d, correct: %d\n", psum, i*j);
+            }
         }
-    }
+    }*/
 
-    in_width = 4;
+    /*in_width = 4;
     weight_width = 4;
     s_weight = 1;
     s_in = 1;
-    for (i = -8; i <= 7; i++) {
-        for (j = -8; j <= 7; j++) {
-            bitfusion((int4) i, (int4) j, s_in, s_weight, in_width, weight_width, psum);
-            if ((uint8)psum != i*j) return 1;                
+    for (k = -8; k <= 7; k++) {
+        for (l = -8; l <= 7; l++) {
+            bitfusion(k, l, s_in, s_weight, in_width, weight_width, psum);
+            if ((int8)psum != k*l) return 1;
         }
-    }
+    }*/
+
+    bitfusion(15, 15, 0, 0, 4, 4, &psum);
+    if (psum != 225) return 1;
+
+    bitfusion(-8, -8, 1, 1, 4, 4, &psum);
+    if ((int8)psum != 64) return 1;
+
+    bitfusion(-8, 7, 1, 1, 4, 4, &psum);
+    if ((int8)psum != -56) return 1;
+
     return 0;
 }
