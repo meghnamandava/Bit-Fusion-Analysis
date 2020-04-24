02157e23df7bc960a33c0e1e6e2a30733a80fcda
Meghna Mandava <meghnamandava@berkeley.edu>
2020-04-24 14:54:40 -0700

    forgot psum_in

--- src_8b_mult/fusion_unit.v
+++ src_8b_mult/fusion_unit.v
@@ -2,6 +2,7 @@ module fusion_unit (
     input clk,
     input [7:0] in,
     input [7:0] weight,
+    input [18:0] psum_in,
     input s_in,
     input s_weight,
     output reg [18:0] psum_fwd
@@ -9,13 +10,13 @@ module fusion_unit (
 
     always @(posedge clk) begin
         if (s_in == 1'b1 && s_weight == 1'b1) begin
-            psum_fwd <= $signed(in) * $signed(weight);
+            psum_fwd <= ($signed(in) * $signed(weight)) + psum_in;
         end else if (s_in == 1'b1 && s_weight == 1'b0) begin
-            psum_fwd <= $signed(in) * weight;
+            psum_fwd <= ($signed(in) * weight) + psum_in;
         end else if (s_in == 1'b0 && s_weight == 1'b1) begin
-            psum_fwd <= in * $signed(weight);
+            psum_fwd <= (in * $signed(weight)) + psum_in;
         end else if (s_in == 1'b0 && s_weight== 1'b0) begin
-            psum_fwd <= in * weight;
+            psum_fwd <= (in * weight) + psum_in;
         end
     end    
 
