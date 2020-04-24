d1418f306ea31ce8c8bebd37d7388872f9695aa0
Meghna Mandava <meghnamandava@berkeley.edu>
2020-04-23 22:20:49 -0700

    compare 4b synthesis

--- src_4b/bitfusion_top.v
+++ src_4b/bitfusion_top.v
@@ -1,7 +1,7 @@
 `timescale 1ns/1ns
 
 module bitfusion_top (
-        input CLK_125MHZ_FPGA,
+        input clk,
         input [3:0] in,
         input [3:0] weight,
         input [2:0] in_width,
@@ -12,9 +12,15 @@ module bitfusion_top (
     );
 
     //need to mux inputs??
+    reg [7:0] in_reg;
+    reg [7:0] weight_reg;
+    reg [3:0] in_width_reg;
+    reg [3:0] weight_width_reg;
+    reg s_in_reg;
+    reg s_weight_reg;
 
     fusion_unit fu0(
-        .clk(CLK_125MHZ_FPGA),
+        .clk(clk),
         .in(in),
         .weight(weight),
         .in_width(in_width),
